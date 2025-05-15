f30ccf6150e87151465cd6e97874f9b5ab2ae223
AldoRatmawan <aldo.ratmawan9999@gmail.com>
2025-05-15 10:32:17 +0700

    update wss

--- main.ts
+++ main.ts
@@ -5,6 +5,25 @@ const app = new Hono()
 app.get('/', (c) => {
   return c.json({
     message: 'Hello, Komuitas IOT!'})
-})
+});
+
+app.get("/ws", (c) => {
+  const { socket, response } = Deno.upgradeWebSocket(c.req.raw);
+
+  socket.onopen = () => {
+    console.log("🔌 WebSocket connected");
+  };
+
+  socket.onmessage = (e) => {
+    console.log("📨 Message received:", e.data);
+    socket.send("👋 Hello back from server!");
+  };
+
+  socket.onclose = () => {
+    console.log("❌ WebSocket closed");
+  };
+
+  return response;
+});
 
 Deno.serve({ port: 7500 } ,app.fetch)
